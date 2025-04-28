ba5e39748aa9d6ba7081c5f288c7534d64263c9f
YusupkinaRita <yusupkina.rita@gmail.com>
2025-04-28 05:54:28 +0300

    mhm

---
+++ Marks.h
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "DatValue.h"
+#include <cstdint>
+
+
+class Marks:public DatValue{
+private:
+    uint16_t _math;
+    uint16_t _cs;
+    uint16_t _eng;
+    uint16_t _phys;
+
+public:
+    Marks(uint16_t math, uint16_t cs, uint16_t eng, uint16_t phys){
+        _math=math;
+        _cs=cs;
+        _eng=eng; 
+        _phys=phys;
+    }
+    Marks(){
+        _math=0;
+        _cs=0;
+        _eng=0; 
+        _phys=0;
+    }
+
+    void Print(std::ostream &os)const override{
+        os<<"math mark :"<<_math<<" ";
+        os<<"cs mark :"<<_cs<<" ";
+        os<<"eng mark :"<<_eng<<" ";
+        os<<"phys mark :"<<_phys<<std::endl;
+    }
+
+    Marks* GetCopy() override{
+        Marks* copy=new Marks(_math, _cs, _eng, _phys);
+        return copy;
+
+    }
+
+
+    
+
+
+
+
+};
